846a36262f5b79106345ee1d41e5e5d303147aac
Suraj <surajgaikwad0287@gmail.com>
2022-12-23 23:36:43 +0530

    morris inorder

--- MorrisPreorder.java
+++ MorrisPreorder.java
@@ -15,12 +15,13 @@ import java.util.ArrayList;
 public class MorrisPreorder{
     
 
-    ArrayList<Integer> morisTraversal(Node root){
+    static ArrayList<Integer> morisTraversal(Node root){
         ArrayList<Integer> list=new ArrayList<>();
         Node curr=root;
         while(curr!=null){
             if(curr.left==null){
                 list.add(curr.data);
+                curr=curr.right;
             }else{
                 Node prev=curr.left;
                 while(prev.right!=null && prev.right!=curr){
@@ -41,6 +42,16 @@ public class MorrisPreorder{
         return list;
     }
     public static void main(String[] args) {
-        
+        Node root =new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.left.right.right=new Node(6);
+        ArrayList<Integer> list=morisTraversal(root);
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+
     }
 }
